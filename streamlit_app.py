5740089919cdbd6ff0cd35b4e82347d62c5aba94
ravidhanisetty <142518763+ravidhanisetty@users.noreply.github.com>
2023-08-17 11:37:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My Parents new Health Dinner')
 
+streamlit.header('Brefast Menu')
+
+streamlit.text('Omega 3 & Blueberry Omlet')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled free range Eggs')
 
