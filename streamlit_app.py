f9af04f38a592060fae73d099f3cb84a8c4003be
ravidhanisetty <142518763+ravidhanisetty@users.noreply.github.com>
2023-08-18 22:34:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # storing data in columnar format usinf the dataframe?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
-
+requirements.txt
 
 
 
