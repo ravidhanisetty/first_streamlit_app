ec8f17bb29adc5a378c9881b17547a72dbdeec94
ravidhanisetty <142518763+ravidhanisetty@users.noreply.github.com>
2023-08-18 23:31:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import url.error
+from urllib.error import urlerror
 
 
 streamlit.title('My Parents new Health Dinner')
